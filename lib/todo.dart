823b653ed10410ed8488111855afea030bf4efb0
CarlosTSilva <carlosteixeira.tc@hotmail.com>
2023-10-19 21:17:12 -0300

    remove itens not using

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 class Todo {
   final int id;
   final String title;
@@ -10,14 +8,6 @@ class Todo {
     required this.done,
   });
 
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'id': id,
-      'title': title,
-      'done': done,
-    };
-  }
-
   factory Todo.fromMap(Map<String, dynamic> map) {
     return Todo(
       id: map['id'] as int,
@@ -25,9 +15,4 @@ class Todo {
       done: map['done'] as int,
     );
   }
-
-  String toJson() => json.encode(toMap());
-
-  factory Todo.fromJson(String source) =>
-      Todo.fromMap(json.decode(source) as Map<String, dynamic>);
 }
--- test/todo_test.dart
+++ test/todo_test.dart
@@ -10,23 +10,9 @@ void main() {
         done: 0,
       );
 
-      final todoMap = todo.toMap();
+      final todoMap = {"id": 1, "title": "Buy groceries", "done": 0};
+
       final newTodo = Todo.fromMap(todoMap);
-
-      expect(newTodo.id, equals(todo.id));
-      expect(newTodo.title, equals(todo.title));
-      expect(newTodo.done, equals(todo.done));
-    });
-
-    test('should convert to and from JSON', () {
-      final todo = Todo(
-        id: 1,
-        title: 'Buy groceries',
-        done: 0,
-      );
-
-      final todoJson = todo.toJson();
-      final newTodo = Todo.fromJson(todoJson);
 
       expect(newTodo.id, equals(todo.id));
       expect(newTodo.title, equals(todo.title));
